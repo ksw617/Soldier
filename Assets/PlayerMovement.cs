9c6f9997a3fb62e90b2d5c6dc3114db26d536ec0
Kwak <ksw617@gmail.com>
2025-02-04 14:16:05 +0900

    Initial commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float moveSpeed;
+    private Vector2 moveDirection;
+
+    public InputActionReference 
+    
+}
